41915eaf428e2641d07c9783607ecc216c8a5510
AmitSheoran-557 <amit22082006@gmail.com>
2025-02-11 16:27:23 +0530

    update code

--- src/components/dashboard/MultiImage.tsx
+++ src/components/dashboard/MultiImage.tsx
@@ -10,10 +10,17 @@ const MultiImage: React.FC = () => {
   const handleFiles = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files) {
-      const fileArr = Array.from(files).map((file) => ({
-        url: URL.createObjectURL(file),
-      }));
-      setImages((prevImages) => [...prevImages, ...fileArr]);
+      const fileArr: any = [];
+      Array.from(files).forEach((file) => {
+        if (file.type.startsWith("image/")) {
+          fileArr.push({
+            url: URL.createObjectURL(file),
+          });
+        } else {
+          alert("Please upload a valid image file.");
+        }
+      });
+      setImages((prevImages: any) => [...prevImages, ...fileArr]);
     }
   };
 
@@ -22,7 +29,7 @@ const MultiImage: React.FC = () => {
       <label htmlFor="image" className='bg-black rounded-lg px-4 py-2 mb-4 mx-auto flex max-w-56 justify-center items-center text-white cursor-pointer text-3xl'>
         Upload Image
       </label>
-      <input type="file" multiple onChange={handleFiles} id='image' hidden />
+      <input type="file" accept='image/*' multiple onChange={handleFiles} id='image' hidden />
       <div className='mt-5 flex gap-4 flex-wrap max-w-[1140px] w-full mx-auto justify-center'>
         {images.map((item, index) => (
           <img className='max-w-80' key={index} src={item.url} alt={`selected-img-${index}`} />
